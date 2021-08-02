049f9acc59a7f91a93ef6014a6d706e78f5d463f
9831065 <najafizadeh21@gmail.com>
2021-08-02 11:03:01 +0430

    A migrate problem fixed

--- TodoList/database/migrations/2021_08_02_062028_create_todos_table.php
+++ TodoList/database/migrations/2021_08_02_062028_create_todos_table.php
@@ -15,6 +15,8 @@ class CreateTodosTable extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->string('todo');
+            $table->boolean('completed')->default(FALSE);
             $table->timestamps();
         });
     }
