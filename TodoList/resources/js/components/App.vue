b1bc32cc51eee59b15abc7d091037c30ab0c2f66
9831065 <najafizadeh21@gmail.com>
2021-08-03 10:48:20 +0430

    App component updated

--- TodoList/resources/js/components/App.vue
+++ TodoList/resources/js/components/App.vue
@@ -1,11 +1,29 @@
 <template>
-    <div class="container">
-        This is an app
+  <div>
+    <h1 style="margin-top: 200px; text-align: center">Todo List</h1>
+    <div class="todoListContainer">
+        <add-item-form />
+        <list-view />
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'App'
-    }
+import AddItemForm from "./AddItemForm.vue"
+import ListView from "./ListView.vue"
+
+export default {
+  components: {
+      AddItemForm,
+      ListView
+  }
+};
 </script>
+
+<style scoped>
+.todoListContainer {
+    width: 400px;
+    margin: auto;
+    margin-top: 50px;
+}
+</style>
--- TodoList/resources/views/welcome.blade.php
+++ TodoList/resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
     </head>
     <body class="antialiased">
         <div id="app">
-            <App />
+            <app></app>
         </div>
     </body>
     <script src="{{ mix('js/app.js') }}"></script>
